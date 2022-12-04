057bad89c580ae81232cbb2595378b21b8e45967
Pavel Makarov <25.makarovv@gmail.com>
2022-12-04 20:03:31 +0600

    django

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-app_name = 'users'
+
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('contacts/', views.contact, name='contacts'),
@@ -9,7 +9,7 @@ urlpatterns = [
     path('form/', views.add_form, name='form'),
     path('form/postuser/', views.add_form, name='postuser'),
     path('database/', views.Data_form.as_view(), name='data'),
-    path('database_detail/<int:id>/', views.DataDetailView.as_view(), name='show_detail'),
+    path('database/<int:id>/', views.DataDetailView.as_view(), name='show_detail'),
     path('login/', views.user_login, name='login'),
     path('guides/', views.guides, name='guides'),
 ]
